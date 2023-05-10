94d0b68218aa10916fa3ff136816e183fd47d989
scdiez <70489460+scdiez@users.noreply.github.com>
2023-05-10 11:35:54 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -5,6 +5,8 @@
 #define dirPin 2
 //#define enPin 8
 
+void move_motor1 (uint16_t, uint8_t);
+
 int main(void)
 {
     DDRD |= (1 << DDD2) | (1 << DDD5); // Set dirPin and stepPin as output
@@ -12,23 +14,25 @@ int main(void)
     //PORTB &= ~(1 << PORTB0); // Set enPin LOW to enable the driver
 
     while (1) {
-        /*clockwise movement
-        PORTD |= (1 << PORTD2); // Set dirPin HIGH to move in a particular direction
-        for (int x = 0; x < 800; x++) {
-            PORTD |= (1 << PORTD5); // Set stepPin HIGH
-            _delay_us(500);
-            PORTD &= ~(1 << PORTD5); // Set stepPin LOW
-            _delay_us(500);
-        }
-        _delay_ms(1000); // One second delay*/
-        PORTD &= ~(1 << PORTD2); // Set dirPin LOW to change direction of rotation
-        for (int x = 0; x < 800; x++) {
-            PORTD |= (1 << PORTD5); // Set stepPin HIGH
-            _delay_us(500);
-            PORTD &= ~(1 << PORTD5); // Set stepPin LOW
-            _delay_us(500);
-        }
-        _delay_ms(1000);
+        move_motor1(800,1);
+        _delay_ms(500);
+        move_motor1(800,0);
+        _delay_ms(500);
     }
     return 0;
+}
+
+void move_motor1 (uint16_t steps, uint8_t direction){
+    if (direction){
+        PORTD |= (1 << PORTD2); // Set dirPin HIGH to move clockwise
+    }
+    else{
+        PORTD |= ~(1 << PORTD2); // Set dirPin LOW to move counterclockwise
+    }
+    for (int x = 0; x <= steps; x++) {
+            PORTD |= (1 << PORTD5); // Set stepPin HIGH
+            _delay_us(500);
+            PORTD &= ~(1 << PORTD5); // Set stepPin LOW
+            _delay_us(500);
+        }
 }
