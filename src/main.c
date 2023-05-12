08248031b13629d8d314bb2bc686c38c5a646016
scdiez <70489460+scdiez@users.noreply.github.com>
2023-05-12 11:56:29 +0200

    Binary but not working

--- src/main.c
+++ src/main.c
@@ -3,29 +3,28 @@
 
 #define stepPin 5
 #define dirPin 2
+#define dirPin2 4
 //#define enPin 8
 
 int main(void)
 {
-    DDRD |= (1 << DDD2) | (1 << DDD5); // Set dirPin and stepPin as output
-    DDRB |= (1 << DDB0); // Set enPin as output
-    //PORTB &= ~(1 << PORTB0); // Set enPin LOW to enable the driver
+    DDRD |= (1 << DDD2) | (1 << DDD5) | (1 << DDD4)| (1 << DDD5); // Set dirPin and stepPin as output
 
     while (1) {
-        /*clockwise movement
-        PORTD |= (1 << PORTD2); // Set dirPin HIGH to move in a particular direction
+        //clockwise movement
+        PORTD = 0b00010100; // Set dirPin HIGH to move in a particular direction
         for (int x = 0; x < 800; x++) {
-            PORTD |= (1 << PORTD5); // Set stepPin HIGH
+            PORTD |= (1 << PORTD5)| (1 << PORTD6); // Set stepPin HIGH
             _delay_us(500);
-            PORTD &= ~(1 << PORTD5); // Set stepPin LOW
+            PORTD &= ~(1 << PORTD5)| ~(1 << PORTD6); // Set stepPin LOW
             _delay_us(500);
         }
-        _delay_ms(1000); // One second delay*/
-        PORTD &= ~(1 << PORTD2); // Set dirPin LOW to change direction of rotation
+        _delay_ms(1000); // One second delay
+        PORTD = 0b00000000; // Set dirPin LOW to change direction of rotation
         for (int x = 0; x < 800; x++) {
-            PORTD |= (1 << PORTD5); // Set stepPin HIGH
+            PORTD |= (1 << PORTD5)| (1 << PORTD6); // Set stepPin HIGH
             _delay_us(500);
-            PORTD &= ~(1 << PORTD5); // Set stepPin LOW
+            PORTD &= ~(1 << PORTD5)| ~(1 << PORTD6); // Set stepPin LOW
             _delay_us(500);
         }
         _delay_ms(1000);
