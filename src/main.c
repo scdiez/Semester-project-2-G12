82bf5266dee49740c6b95379a439aa2020c0ba93
scdiez <70489460+scdiez@users.noreply.github.com>
2023-05-15 12:04:12 +0200

    commit

--- src/main.c
+++ src/main.c
@@ -1,6 +1,11 @@
-#include <usart.h>
+#include <stdio.h>
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stdlib.h>
+#include "i2cmaster.h"          //"" are used as these are user defined libraries
+#include "lcd.h"
+#include "usart.h"
+
 #define F_CPU 16000000UL
 
 //DEFINE BAUDRATE AS 9600
@@ -25,32 +30,29 @@ int main(void)
     TCCR0A |= (1<<WGM01); //Timer mode to CTC
     OCR0A = 0xF9; //set value to count to
     
-
-    // Enable internal pull-up resistor for Pin 3
-    PORTB |= (1 << PORTD3);
+    PORTB |= ~(1 << PORTD2); //set pin 2 to low 
+    PORTB |= (1 << PORTD2); //pull-up resistors in echo pin 
 
     while (1) {
-        // Set Pin 9 to high
+        // Set Pin 9 to high (Send signal)
         PORTB |= (1 << PORTD2);
         _delay_ms(10);
-        PORTB |= ~(1 << PORTD2);
-
-        //measure pulse duration
-        if (PORTD3){
-            TCCR0B |= (1<<CS01) | (1<<CS00); //Start the timer and prescaler to 64
-            while((TIFR0 & (1<<OCF0A))==0){//wait the overflow event
-            }
-            //reset overflow flag 
-            TIFR0 = (1<<OCF0A);
-            timer ++;
-            if(PORTB0 == 0){
-                miliseconds = timer;
-                timer = 0;
-            }
+        PORTB |= ~(1 << PORTD2); //set pin 2 to low (stop signal)
+        TCCR0A |= (1<<WGM01); //Timer mode to CTC
+        OCR0A = 0xF9; //set value to count to
+        TCCR0B |= (1<<CS01) | (1<<CS00); //Start the timer and prescaler to 64
+        while((TIFR0 & (1<<OCF0A))==0){//wait the overflov event
+        }
+        //reset overflow flag 
+        TIFR0 = (1<<OCF0A);
+        timer ++;
+        if(PORTB == 0){
+            miliseconds = timer;
+            timer = 0;
+            distanceincm = 0.17*1000*miliseconds;
+            printf("%d", distanceincm);
         }
 
-        distanceincm = 0.17*1000*miliseconds;
-        printf("%f", distanceincm);
     }
     return 0;
 }
