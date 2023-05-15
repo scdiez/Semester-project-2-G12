fbc80635004fd8e834d4323841e2fbfb8b0d90bd
scdiez <70489460+scdiez@users.noreply.github.com>
2023-05-15 15:42:31 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -5,53 +5,78 @@
 #include "i2cmaster.h"          //"" are used as these are user defined libraries
 #include "lcd.h"
 #include "usart.h"
+#include <avr/interrupt.h>
 
 #define F_CPU 16000000UL
 
 //DEFINE BAUDRATE AS 9600
 
 #define trigPin 2
-#define echopin 3
+#define echopin 4
 
-volatile float timer;
-volatile float miliseconds;
-volatile float distanceincm;
+volatile unsigned long pulse_start;
+volatile unsigned long pulse_end;
+volatile unsigned long pulse_duration;
+volatile int distance;
+
+void start_pulse() {
+    pulse_start = TCNT1; // Record the timer value at the start of the pulse
+}
+
+void end_pulse() {
+    pulse_end = TCNT1; // Record the timer value at the end of the pulse
+    pulse_duration = pulse_end - pulse_start; // Calculate the pulse duration
+}
 
 
 int main(void)
 {
+    sei(); // Enable global interrupts
     uart_init();
     io_redirect();
-    //sensor configuration
-    DDRD |= (1 << DDD2); // Set trig as output
-    DDRD &= ~(1 << DDD3); //Set echo pin as input 
 
-    //timer configuration
-    TCCR0A |= (1<<WGM01); //Timer mode to CTC
-    OCR0A = 0xF9; //set value to count to
-    
-    PORTB |= ~(1 << PORTD2); //set pin 2 to low 
+    DDRD |= (1 << DDD2);
+
+    // Set echoPin as an input
+    DDRD &= ~(1 << DDD4);
+
+    // Enable internal pull-up resistor for echoPin
+    PORTD |= (1 << PORTD4);
 
     while (1) {
-        PORTD |= (1 << PORTD2); // Set Pin 2 to high (Send signal)
-        PORTD |= (1 << PORTD3); //Set pin 3 to high 
-        _delay_ms(10);
-        PORTD |= ~(1 << PORTD2); //set pin 2 to low (stop signal)
-        TCCR0A |= (1<<WGM01); //Timer mode to CTC
-        OCR0A = 0xF9; //set value to count to
-        TCCR0B |= (1<<CS01) | (1<<CS00); //Start the timer and prescaler to 64
-        while((TIFR0 & (1<<OCF0A))==0){//wait the overflov event
-        }
-        //reset overflow flag 
-        TIFR0 = (1<<OCF0A);
-        timer ++;
-        if(PORTD3 == 0){
-            miliseconds = timer;
-            timer = 0;
-            distanceincm = 0.17*1000*miliseconds;
-            printf("%d", distanceincm);
-        }
+        // Clears the trigPin
+        PORTD &= ~(1 << PORTD2);
+        _delay_us(2);
 
+        // Sets the trigPin on HIGH state for 10 microseconds
+        PORTD |= (1 << PORTD2);
+        _delay_us(10);
+        PORTD &= ~(1 << PORTD2);
+
+        // Wait for the falling edge on echoPin
+        while ((PIND & (1 << PIND4)) == 0) {}
+
+        // Start Timer/Counter1
+        TCCR1B |= (1 << CS11); // Set prescaler to 8
+
+        // Record the start time of the pulse
+        start_pulse();
+
+        // Wait for the rising edge on echoPin
+        while (PIND & (1 << PIND4)) {}
+
+        // Record the end time of the pulse
+        end_pulse();
+
+        // Stop Timer/Counter1
+        TCCR1B = 0;
+
+        // Calculate the distance
+        distance = pulse_duration * 0.034 / 2;
+
+        // Prints the distance on the Serial Monitor
+        printf("Distance: %d\n", distance);
     }
+
     return 0;
 }
