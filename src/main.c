ea9608899465c99464d882093d82e86b896b4bd6
scdiez <70489460+scdiez@users.noreply.github.com>
2023-05-15 14:24:59 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,30 +1,30 @@
 #include <avr/io.h>
 #include <stdio.h>
 #include <util/delay.h>
-#include <stdlib.h>
 #include "i2cmaster.h"
 #include "lcd.h"
 #include "usart.h"
 
-int randnumx (void);
-int randnumy (void);
-int target_x,target_y;
-int current_x=0;
-int current_y=0;
-int bla;
+int main (){
+uart_init(); // open the communication to the microcontroller
+io_redirect();
 
-int main(void) {
-    srand(0);
-    printf("%d \n", target_x);
-    printf("%d \n", target_y);
+//setting buttons
+DDRC = 0xF0; //buttons inputs
+PORTC = 0x3F; //Enable internal pullups at PC0..3
+
+while(1){
+  if (PINC == 0b00111110){ //first button pressed?
+	  printf("DI0 pressed");
+  } 
+  if (PINC == 0b00111101){ //second button pressed?
+	  printf("DI1 pressed");
+  }  
+    if (PINC == 0b00111011){ //third button pressed?
+	  printf("DI2 pressed");
+  } 
+    if (PINC == 0b00110111){ //fourth button pressed?
+	  printf("DI3 pressed");
+  } 
 }
-
-int randnumx (void){
-    target_x = rand()%50; //generate number for position SET LIMITS ONCE TESTED
-    return target_x;
-}
-
-int randnumy (void){
-    target_y = rand()%50; //generate number for position SET LIMITS ONCE TESTED
-    return target_y;
 }
