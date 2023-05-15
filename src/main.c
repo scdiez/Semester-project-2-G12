d35ee100c6c585a973511379d480fbb0103d5afa
scdiez <70489460+scdiez@users.noreply.github.com>
2023-05-15 11:10:12 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,7 +1,29 @@
 #include <avr/io.h>
+#include <stdio.h>
 #include <util/delay.h>
+#include <stdlib.h>
+#include "i2cmaster.h"
+#include "lcd.h"
+#include "usart.h"
+
+int randnumx (void);
+int randnumy (void);
+int target_x,target_y;
+int current_x=0;
+int current_y=0;
 
 int main(void) {
+    srand(0);
+    printf("%d \n", target_x);
+    printf("%d \n", target_y);
+}
 
-//Edit by smrithi
+int randnumx (void){
+    target_x = rand()%50; //generate number for position SET LIMITS ONCE TESTED
+    return target_x;
+}
+
+int randnumy (void){
+    target_y = rand()%50; //generate number for position SET LIMITS ONCE TESTED
+    return target_y;
 }
