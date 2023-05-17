91bb0b5e90305fc31c8b6e28ff58906653269d49
scdiez <70489460+scdiez@users.noreply.github.com>
2023-05-17 13:37:24 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stdio.h>
+#include "usart.h"
 
 #define stepPin 5
 #define dirPin 2
@@ -7,27 +9,29 @@
 
 int main(void)
 {
-    DDRD |= (1 << DDD2) | (1 << DDD5); // Set dirPin and stepPin as output
+    //uart_init();
+    //io_redirect();
+    DDRD |= (1 << DDD2) | (1 << DDD5) |  (1 << DDD4); // Set dirpin and stepPin as output
     DDRB |= (1 << DDB0); // Set enPin as output
     PORTB &= ~(1 << PORTB0); // Set enPin LOW to enable the driver
 
     while (1) {
-        PORTD |= (1 << PORTD2); // Set dirPin HIGH to move in a particular direction
+        //PORTD |= (1 << PORTD2); // Set dirPin HIGH to move in a particular direction
+        //printf("Clockwise \n");
         for (int x = 0; x < 1600; x++) {
             PORTD |= (1 << PORTD5); // Set stepPin HIGH
             _delay_us(500);
             PORTD &= ~(1 << PORTD5); // Set stepPin LOW
             _delay_us(500);
         }
-        _delay_ms(1000);
-        PORTD &= ~(1 << PORTD2); // Set dirPin LOW to change direction of rotation
+        /*PORTD &= ~(1 << PORTD2); // Set dirPin LOW to change direction of rotation
+        printf("Anticlockwise \n");
         for (int x = 0; x < 1600; x++) {
             PORTD |= (1 << PORTD5); // Set stepPin HIGH
             _delay_us(500);
             PORTD &= ~(1 << PORTD5); // Set stepPin LOW
             _delay_us(500);
-        }
-        _delay_ms(1000);
+        }*/
     }
     return 0;
 }
