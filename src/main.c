e8c243aa9b39cc16994c2c1cd623cbde0cb45b02
scdiez <70489460+scdiez@users.noreply.github.com>
2023-05-22 12:23:21 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -4,54 +4,78 @@
 #define stepPin 5
 #define dirPin 2
 
-void move_clockwise(int);
-void move_counter_clockwise(int);
+void move_clockwise(int, int);
+void move_counter_clockwise(int, int);
+void move_up (int, int);
+void move_down(int, int);
 
 int main(void)
 {
-    DDRD |= (1 << DDD2) | (1 << DDD5); // Set dirPin and stepPin as output
+    DDRD |= (1 << DDD4) | (1 << DDD6); // Set dirPin and stepPin as output
+	DDRD |= (1 << DDD2) | (1 << DDD5);
 
     while (1) {
-        /*PORTD |= (1 << PORTD2); // Set dirPin HIGH to move in a particular direction
-        for (int x = 0; x < 800; x++) {
-            PORTD |= (1 << PORTD5); // Set stepPin HIGH
-            _delay_us(500);
-            PORTD &= ~(1 << PORTD5); // Set stepPin LOW
-            _delay_us(500);
-        }
-        _delay_ms(1000); // One second delay
-        PORTD &= ~(1 << PORTD2); // Set dirPin LOW to change direction of rotation
-        for (int x = 0; x < 800; x++) {
-            PORTD |= (1 << PORTD5); // Set stepPin HIGH
-            _delay_us(500);
-            PORTD &= ~(1 << PORTD5); // Set stepPin LOW
-            _delay_us(500);*/
-			move_clockwise(3000);
-			_delay_ms(500);
-			move_counter_clockwise(3000);
-			_delay_ms(500);
-        }
-
-
+		move_clockwise(3000, 200);
+		_delay_ms(500);
+		move_counter_clockwise(3000, 200);
+		_delay_ms(500);
+		move_up (3000, 200);
+		_delay_ms(500);
+		move_down(3000, 200);
+		_delay_ms(500);
+	}
     return 0;
 }
 
-void move_clockwise(steps){
-	 PORTD |= (1 << PORTD2); // Set dirPin HIGH to move in a particular direction
+void move_clockwise(int steps, int delay){
+	PORTD |= (1 << PORTD4); // Set dirPin HIGH to move in a particular direction
+	PORTD |= (1 << PORTD2); // Set dirPin HIGH to move in a particular direction
         for (int x = 0; x < steps; x++) {
-            PORTD |= (1 << PORTD5); // Set stepPin HIGH
-            _delay_us(100);
-            PORTD &= ~(1 << PORTD5); // Set stepPin LOW
-            _delay_us(100);
+            PORTD |= (1 << PORTD6); // Set stepPin HIGH
+			PORTD |= (1 << PORTD5); // Set stepPin HIGH
+            _delay_us(delay);
+            PORTD &= ~(1 << PORTD6); // Set stepPin LOW
+			PORTD &= ~(1 << PORTD5); // Set stepPin LOW
+            _delay_us(delay);
+			}
+}
+void move_counter_clockwise(int steps, int delay){
+	PORTD &= ~(1 << PORTD4); // Set dirPin LOW to change direction of rotation
+	PORTD &= ~(1 << PORTD2);
+        for (int x = 0; x < steps; x++) {
+            PORTD |= (1 << PORTD6); // Set stepPin HIGH
+			PORTD |= (1 << PORTD5); // Set stepPin HIGH
+            _delay_us(delay);
+            PORTD &= ~(1 << PORTD6); // Set stepPin LOW
+			PORTD &= ~(1 << PORTD5); // Set stepPin LOW
+            _delay_us(delay);
         }
 }
-void move_counter_clockwise(Steps){
-	PORTD &= ~(1 << PORTD2); // Set dirPin LOW to change direction of rotation
-        for (int x = 0; x < Steps; x++) {
-            PORTD |= (1 << PORTD5); // Set stepPin HIGH
-            _delay_us(500);
-            PORTD &= ~(1 << PORTD5); // Set stepPin LOW
-            _delay_us(500);
+
+void move_up(int steps, int delay){
+	PORTD |= (1 << PORTD4); // Set dirPin HIGH to move in a particular direction
+	PORTD &= ~(1 << PORTD2);
+        for (int x = 0; x < steps; x++) {
+            PORTD |= (1 << PORTD6); // Set stepPin HIGH
+			PORTD |= (1 << PORTD5); // Set stepPin HIGH
+            _delay_us(delay);
+            PORTD &= ~(1 << PORTD6); // Set stepPin LOW
+			PORTD &= ~(1 << PORTD5); // Set stepPin LOW
+            _delay_us(delay);
         }
-        _delay_ms(1000);
 }
+
+void move_down(int steps, int delay){
+	PORTD |= (1 << PORTD2); // Set dirPin HIGH to move in a particular direction
+	PORTD &= ~(1 << PORTD4);
+        for (int x = 0; x < steps; x++) {
+            PORTD |= (1 << PORTD6); // Set stepPin HIGH
+			PORTD |= (1 << PORTD5); // Set stepPin HIGH
+            _delay_us(delay);
+            PORTD &= ~(1 << PORTD6); // Set stepPin LOW
+			PORTD &= ~(1 << PORTD5); // Set stepPin LOW
+            _delay_us(delay);
+        }
+}
+
+
