f715df7e25fa37d816a68268946a459c32043e88
scdiez <70489460+scdiez@users.noreply.github.com>
2023-05-23 12:24:17 +0200

    Not working

--- src/main.c
+++ src/main.c
@@ -1,34 +1,63 @@
 #include <avr/io.h>
-#include <stdio.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
-#include <stdlib.h>
-#include "i2cmaster.h"
-#include "lcd.h"
-#include "usart.h"
-#include <time.h>
 
-int target_x,target_y;
-int current_x=0;
-int current_y=0;;
+#define F_CPU 16000000UL  // Define the clock frequency
+#define SPEAKER_PIN 9     // Define the speaker pin
 
-int main(void) {
-    uart_init();
-    io_redirect();
+volatile uint8_t audioData[] = {
+  // Audio data bytes here
+};
 
-    srand(time(0));
-    target_x = rand() % 50;
-    target_y = rand() % 50;
-    
-    printf("Target x: %d \n", target_x);
-    printf("Target y: %d \n", target_y);
-    target_x = rand() % 50;
-    target_y = rand() % 50,
-    
-    printf("Target x: %d \n", target_x);
-    printf("Target y: %d \n", target_y);
-    target_x = rand() % 50;
-    target_y = rand() % 50,
-    
-    printf("Target x: %d \n", target_x);
-    printf("Target y: %d \n", target_y);
+volatile uint32_t audioDataSize = sizeof(audioData);
+volatile uint32_t audioDataIndex = 0;
+
+// Function to initialize the speaker pin
+void init_speaker() {
+  // Set the speaker pin as output
+  DDRD |= (1 << SPEAKER_PIN);
+}
+
+// Function to play a sound sample on the speaker
+void play_sample(uint8_t sample) {
+  // Write the sample value to the speaker pin
+  PORTD = (PORTD & ~(1 << SPEAKER_PIN)) | ((sample >> 1) & 1) << SPEAKER_PIN;
+}
+
+// Timer1 overflow interrupt
+ISR(TIMER1_OVF_vect) {
+  // Check if all audio data has been played
+  if (audioDataIndex >= audioDataSize) {
+    // Reset the audio data index if you want to loop the audio
+    audioDataIndex = 0;
+  }
+
+  // Get the current sample from the audio data
+  uint8_t sample = audioData[audioDataIndex];
+
+  // Play the sample on the speaker
+  play_sample(sample);
+
+  // Increment the audio data index for the next sample
+  audioDataIndex++;
+}
+
+int main() {
+  // Initialize the speaker pin
+  init_speaker();
+
+  // Configure Timer1 for audio playback
+  TCCR1B |= (1 << CS10);  // Set prescaler to 1
+  TIMSK1 |= (1 << TOIE1); // Enable Timer1 overflow interrupt
+
+  // Enable global interrupts
+  sei();
+
+  // Main loop
+  while (1) {
+    // Additional code or tasks can be placed here if needed
+    // The audio playback is handled by the interrupt
+  }
+
+  return 0;
 }
