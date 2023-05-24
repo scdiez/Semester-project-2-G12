f2db1efe775e1bd8efdc1d791215316bddd7ad62
scdiez <70489460+scdiez@users.noreply.github.com>
2023-05-24 15:51:47 +0200

    Pulse duration as time

--- src/main.c
+++ src/main.c
@@ -7,9 +7,7 @@
 #include "usart.h"
 #include <avr/interrupt.h>
 
-#define F_CPU 16000000UL
-
-//DEFINE BAUDRATE AS 9600
+#define F_CPU 16000000UL //DEFINE BAUDRATE AS 9600
 
 #define trigPin 2
 #define echopin 4
@@ -18,6 +16,8 @@ volatile unsigned long pulse_start;
 volatile unsigned long pulse_end;
 volatile unsigned long pulse_duration;
 volatile int distance;
+unsigned int trial_time = 0;
+int counter;
 
 void start_pulse() {
     pulse_start = TCNT1; // Record the timer value at the start of the pulse
@@ -28,7 +28,6 @@ void end_pulse() {
     pulse_duration = pulse_end - pulse_start; // Calculate the pulse duration
 }
 
-
 int main(void)
 {
     sei(); // Enable global interrupts
@@ -36,46 +35,41 @@ int main(void)
     io_redirect();
 
     DDRD |= (1 << DDD2);
+    DDRD &= ~(1 << DDD4);     // Set echoPin as an input
+    PORTD |= (1 << PORTD4);     // Enable internal pull-up resistor for echoPin
 
-    // Set echoPin as an input
-    DDRD &= ~(1 << DDD4);
-
-    // Enable internal pull-up resistor for echoPin
-    PORTD |= (1 << PORTD4);
 
     while (1) {
-        // Clears the trigPin
-        PORTD &= ~(1 << PORTD2);
+        PORTD &= ~(1 << PORTD2); // Clears the trigPin
         _delay_us(2);
-
-        // Sets the trigPin on HIGH state for 10 microseconds
-        PORTD |= (1 << PORTD2);
+        PORTD |= (1 << PORTD2);    // Sets the trigPin on HIGH state for 10 microseconds
         _delay_us(10);
         PORTD &= ~(1 << PORTD2);
-
-        // Wait for the falling edge on echoPin
-        while ((PIND & (1 << PIND4)) == 0) {}
-
+        while ((PIND & (1 << PIND4)) == 0) {}  // Wait for the falling edge on echoPin
         // Start Timer/Counter1
         TCCR1B |= (1 << CS11); // Set prescaler to 8
+        start_pulse();        // Record the start time of the pulse
+        while (PIND & (1 << PIND4)) {} // Wait for the rising edge on echoPin
+        end_pulse();     // Record the end time of the pulse
+        TCCR1B = 0;   // Stop Timer/Counter1
 
-        // Record the start time of the pulse
-        start_pulse();
+		trial_time = trial_time + pulse_duration;
+        distance = pulse_duration * 0.017 / 2; // Calculate the distance
+		printf("%u", trial_time);
 
-        // Wait for the rising edge on echoPin
-        while (PIND & (1 << PIND4)) {}
+		if (trial_time>=65000){
+			counter++;
+			trial_time=0;
+		}
+        if (distance < 9.5) {
+            printf("Nice shot! \n");
+        }
 
-        // Record the end time of the pulse
-        end_pulse();
 
-        // Stop Timer/Counter1
-        TCCR1B = 0;
-
-        // Calculate the distance
-        distance = pulse_duration * 0.034 / 2;
-
-        // Prints the distance on the Serial Monitor
-        printf("Distance: %d\n", distance);
+        if (distance > 9.5 && counter>=10) {
+            printf("Try Again \n");
+			counter++;
+        }
     }
 
     return 0;
