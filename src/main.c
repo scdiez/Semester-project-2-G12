4bd894520806a0515a8ce8fbc44bc37693e799cf
scdiez <70489460+scdiez@users.noreply.github.com>
2023-05-26 13:01:11 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -15,72 +15,150 @@
 #define dirPin 2
 #define enPin 8
 
-uint16_t adc_read(uint8_t adc_channel);
 
+int voltagey;
+int voltagex;
+uint16_t adc_result0;
+uint16_t adc_result1;
+int joystickflag;
+int timerOverflow = 0;
+
+uint16_t adc_read(uint8_t adc_channel);
+void joystick();
+
+//functions for motor movement
+void move_left(int, int);
+void move_right(int, int);
+void move_up (int, int);
+void move_down(int, int);
+void change_position(void);
+void zero(void);
+  
 int main (void){
 
     uart_init();
     io_redirect();
 
+    //Motor config
+    DDRD |= (1 << DDD4) | (1 << DDD6); // Set dirPin1 and stepPin1 as output
+	DDRD |= (1 << DDD2) | (1 << DDD5); // Set dirPin2 and stepPin2 as output
+
+    //Joystick config
     DDRB = 1<<5;
     PORTB = 1<<5;
-
-    DDRD |= (1 << DDD2) | (1 << DDD5); // Set dirPin and stepPin as output
-    DDRB |= (1 << DDB0); // Set enPin as output
-    PORTB &= ~(1 << PORTB0); // Set enPin LOW to enable the driver 
-
-    int voltagey;
-    int voltagex;
-
-    uint16_t adc_result0;
-    uint16_t adc_result1;
     ADMUX = (1<<REFS0); //Select vref = avcc
     ADCSRA = (1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)|(1<<ADEN); //set prescaler to 128 and turn on adc module
+
+    while (1){}
+    {
+        joystick();
+    }
     
-    while(1){
-        adc_result0 = adc_read(ADC_PIN0); //voltage depends on joystick stage so return voltage read
-        adc_result1 = adc_read(ADC_PIN1);
+    
 
-        voltagex = (adc_result1/100);
-        voltagey = (adc_result0/100);
+    
+}
 
-        if(voltagex>5){
-            printf("Clockwise \n");
-            PORTD |= (1 << PORTD2); // Set dirPin HIGH to move in a particular direction
-                for (int x = 0; x < 300; x++) {
-                PORTD |= (1 << PORTD5); // Set stepPin HIGH
-                _delay_us(500);
-                PORTD &= ~(1 << PORTD5); // Set stepPin LOW
-                _delay_us(500);
-            }
+void move_left(int steps, int delay){
+	PORTD |= (1 << PORTD4); // Set dirPin HIGH to move in a particular direction
+	PORTD |= (1 << PORTD2); // Set dirPin HIGH to move in a particular direction
+        for (int x = 0; x < steps; x++) {
+            PORTD |= (1 << PORTD6); // Set stepPin HIGH
+			PORTD |= (1 << PORTD5); // Set stepPin HIGH
+            _delay_us(delay);
+            PORTD &= ~(1 << PORTD6); // Set stepPin LOW
+			PORTD &= ~(1 << PORTD5); // Set stepPin LOW
+            _delay_us(delay);
+			}
+}
+void move_right(int steps, int delay){
+	PORTD &= ~(1 << PORTD4); // Set dirPin LOW to change direction of rotation
+	PORTD &= ~(1 << PORTD2);
+        for (int x = 0; x < steps; x++) {
+            PORTD |= (1 << PORTD6); // Set stepPin HIGH
+			PORTD |= (1 << PORTD5); // Set stepPin HIGH
+            _delay_us(delay);
+            PORTD &= ~(1 << PORTD6); // Set stepPin LOW
+			PORTD &= ~(1 << PORTD5); // Set stepPin LOW
+            _delay_us(delay);
         }
-        if(voltagex<=5){
-            PORTD &= ~(1 << PORTD5); // Set stepPin LOW
+}
+
+void move_up(int steps, int delay){
+	PORTD |= (1 << PORTD4); // Set dirPin HIGH to move in a particular direction
+	PORTD &= ~(1 << PORTD2);
+        for (int x = 0; x < steps; x++) {
+            PORTD |= (1 << PORTD6); // Set stepPin HIGH
+			PORTD |= (1 << PORTD5); // Set stepPin HIGH
+            _delay_us(delay);
+            PORTD &= ~(1 << PORTD6); // Set stepPin LOW
+			PORTD &= ~(1 << PORTD5); // Set stepPin LOW
+            _delay_us(delay);
         }
+}
 
-        if(voltagey>5){
-            printf("Anticlockwise \n");
-            PORTD |= ~(1 << PORTD2); // Set dirPin LOW to move in a particular direction
-            for (int x = 0; x < 300; x++) {
-                PORTD |= (1 << PORTD5); // Set stepPin HIGH
-                _delay_us(500);
-                PORTD &= ~(1 << PORTD5); // Set stepPin LOW
-                _delay_us(500);
-            }
+void move_down(int steps, int delay){
+	PORTD |= (1 << PORTD2); // Set dirPin HIGH to move in a particular direction
+	PORTD &= ~(1 << PORTD4);
+        for (int x = 0; x < steps; x++) {
+            PORTD |= (1 << PORTD6); // Set stepPin HIGH
+			PORTD |= (1 << PORTD5); // Set stepPin HIGH
+            _delay_us(delay);
+            PORTD &= ~(1 << PORTD6); // Set stepPin LOW
+			PORTD &= ~(1 << PORTD5); // Set stepPin LOW
+            _delay_us(delay);
         }
+}
 
-        if(voltagey<=5){
-            PORTD &= ~(1 << PORTD5); // Set stepPin LOW
-        }
-
-
-    }
-    }
-
-    uint16_t adc_read(uint8_t adc_channel){
+uint16_t adc_read(uint8_t adc_channel){
     ADMUX &= 0xf0; //clear any previously used channel keeping internal reference
     ADMUX |= adc_channel; //set the desired channel 
     ADCSRA |= (1<<ADSC); //start a conversion
     while ((ADCSRA&(1<<ADSC))); //wait for conversion to complete
     return ADC; //return result as a 16 bit unsigned int
     }
+
+void joystick(void){
+	joystickflag = 1;
+	while(joystickflag == 1){
+		adc_result0 = adc_read(ADC_PIN0); //voltage depends on joystick stage so return voltage read
+		adc_result1 = adc_read(ADC_PIN1);
+		voltagex = (adc_result1/100);
+		voltagey = (adc_result0/100);
+		timerOverflow = 0;
+
+		while(voltagex==5 && voltagey==5 && timerOverflow <= 5000){
+				TCCR0A |= (1 << WGM01);
+				// Set the compare value for 1 ms 
+				OCR0A = 0xF9;
+				//Set the prescaler to 64 and start timer
+				TCCR0B |= (1 << CS01) | (1 << CS00);
+				// Enable the output compare A match interrupt
+				while ( (TIFR0 & (1 << OCF0A) ) == 0){  // wait for the overflow event
+			}
+			timerOverflow++;
+			if(timerOverflow >= 4998){
+				joystickflag=0;
+			}
+		}
+		// reset the overflow flag
+		
+		TIFR0 = (1 << OCF0A);
+		
+		if (voltagex>=6){
+			move_right(5, 400);
+		}
+		if (voltagex<4){
+			move_left(5, 400);
+		}
+		if(voltagey>=6){
+			move_up(5, 400);
+		}
+		if (voltagey<4){
+			move_down(5, 400);
+		}
+	 // Reset the timer overflow variable
+    timerOverflow = 0;
+        }
+    }
+    
