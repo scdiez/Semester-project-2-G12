314680f5d7420869f6e6d5dbdce1c53f01f8ef90
scdiez <70489460+scdiez@users.noreply.github.com>
2023-05-26 13:41:16 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,38 +3,40 @@
 #include <util/delay.h>
 #include "i2cmaster.h"
 #include "lcd.h"
+#include "usart.h"
 
 //Vrx to A0
 //Vry to A1
 
-#define ADC_PIN0 0 //ADC channels we'll use
-#define ADC_PIN1 1 
+#define JOYSTICK_Y 0 //ADC channels we'll use
+#define JOYSTICK_X 1 
 
-uint16_t adc_read(uint8_t adc_channel);
+//Joystick functions
+void joystick ();
+uint16_t adc_read(uint8_t);
+
+//Joystick variables
+int voltagey;
+int voltagex;
+uint16_t adc_resulty;
+uint16_t adc_resultx;
+int joystickflag;
 
 int main (void){
 
-  uart_init();
-  io_redirect();
+    uart_init();
+    io_redirect();
 
-  DDRB = 1<<5;
-  PORTB = 1<<5;
 
-  int voltagey;
-  int voltagex;
+    //Joystick configuration
+    DDRB = 1<<5;
+    PORTB = 1<<5;
+    ADMUX = (1<<REFS0); //Select vref = avcc
+    ADCSRA = (1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)|(1<<ADEN); //set prescaler to 128 and turn on adc module
 
-  uint16_t adc_result0;
-  uint16_t adc_result1;
-  ADMUX = (1<<REFS0); //Select vref = avcc
-  ADCSRA = (1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)|(1<<ADEN); //set prescaler to 128 and turn on adc module
-  while(1){
-    adc_result0 = adc_read(ADC_PIN0); //voltage depends on joystick stage so return voltage read
-    adc_result1 = adc_read(ADC_PIN1);
-    voltagex = (adc_result1/100);
-    printf("Voltage x =%d \n", voltagex);
-    voltagey = (adc_result0/100);
-    printf("Voltage y =%d \n", voltagey);
-  }
+    joystick();
+
+
 }
 
 uint16_t adc_read(uint8_t adc_channel){
@@ -44,3 +46,30 @@ uint16_t adc_read(uint8_t adc_channel){
   while ((ADCSRA&(1<<ADSC))); //wait for conversion to complete
   return ADC; //return result as a 16 bit unsigned int
 }
+
+void joystick (void){
+    joystickflag = 1;
+    while (joystickflag == 1){
+        adc_resultx = adc_read(JOYSTICK_X); //voltage depends on joystick stage so return voltage read (0-1024mV)
+        voltagex = (adc_resultx);
+        
+        adc_resulty = adc_read(JOYSTICK_Y); //voltage depends on joystick stage so return voltage read
+        voltagey = (adc_resulty);
+
+        if(voltagex >=1000){ //set threshold to start moving
+            printf("movemotor right \n");
+        }
+        if(voltagex <=50){
+            printf("movemotor left \n");
+        }
+        if(voltagey >=1000){
+            printf("movemotor up \n");
+        }
+        if(voltagey <=50){
+            printf("movemotor down \n");
+        }
+    }
+    
+}
+
+
