46baf00b230e5c3971fccd71b3c3e381730ce927
scdiez <70489460+scdiez@users.noreply.github.com>
2023-05-29 17:18:55 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,36 +2,32 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include "i2cmaster.h"
-#define BAUDRATE 57600
+#define BAUDRATE 9600
 #define BAUD_PRESCALER (((F_CPU/(BAUDRATE*16UL)))-1)
 #include <stdio.h>
 #include "usart.h"
 
 
 void usart_init(void);
+unsigned char usart_receive(void);
 void usart_send (unsigned char data);
 
 
 
 int main(void){
-  usart_init();
-  io_redirect();
+    usart_init();
+    io_redirect();
+    while(1){
+        usart_send(1);
+        _delay_ms(2000);
+        usart_send(2);
+        _delay_ms(2000);
+        usart_send(3);
+        _delay_ms(1000);
+        usart_send(4);
+        _delay_ms(2000);
+    }
 
-  //Button configuration
-  DDRC = 0xF0;
-	PORTC = 0x3F;
-
-  if (PINC == 0b00111110){
-  usart_send(13); //"You've selected vision single player"
-  _delay_ms(3000);
-  usart_send(7); //"The basketball hoop will now move. You get 3 attempts to score."
-  _delay_ms(4000);
-  usart_send(6);// " Press Button 1 two times when you want to stop playing. Good Luck!" 
-  _delay_ms(4000);
-  usart_send(44); // "you have 8 seconds to get the ball in"
-  _delay_ms(3000);
-  }
-    
 return 0;
 
 }
