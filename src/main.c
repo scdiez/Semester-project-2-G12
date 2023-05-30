68c4f35f5478ed81cb66ace2eb69b8440dd2f20a
scdiez <70489460+scdiez@users.noreply.github.com>
2023-05-30 13:57:46 +0200

    Working function
    Move motor with joystick, end with button and check if ball was shot in

--- src/main.c
+++ src/main.c
@@ -1,16 +1,25 @@
-#include <stdio.h>
 #include <avr/io.h>
+#include <stdio.h>
 #include <util/delay.h>
-#include <stdlib.h>
-#include "i2cmaster.h"          //"" are used as these are user defined libraries
+#include "i2cmaster.h"
 #include "lcd.h"
 #include "usart.h"
 #include <avr/interrupt.h>
+
+//Vrx to A0
+//Vry to A1
 
 #define F_CPU 16000000UL //DEFINE BAUDRATE AS 9600
 
 #define trigPin 8
 #define echopin 9
+
+
+
+#define JOYSTICK_Y 0 //ADC channels we'll use A0
+#define JOYSTICK_X 1 //A1
+#define JOYSTICK_SW 12 //Joystick button D12
+
 
 volatile unsigned long pulse_start;
 volatile unsigned long pulse_end;
@@ -19,29 +28,162 @@ volatile int distance;
 unsigned int trial_time = 0;
 int counter = 0;
 int sensorflag = 0;
+int shotin=0;
 
 void start_pulse(void);
-
 void end_pulse(void);
-
 int detect_ball (void);
 
-int main(void)
-{
-    sei(); // Enable global interrupts
+
+//Joystick functions
+void joystick ();
+uint16_t adc_read(uint8_t);
+
+//functions for motor movement
+void move_left(int, int);
+void move_right(int, int);
+void move_up (int, int);
+void move_down(int, int);
+void change_position(void);
+void zero(void);
+
+//Joystick variables
+int voltagey;
+int voltagex;
+uint16_t adc_resulty;
+uint16_t adc_resultx;
+int joystickflag;
+int buttonstate = 1;
+
+//variables for motor movement 
+int target_x,target_y, move_x, move_y;
+int current_x=0;
+int current_y=0;
+
+int main (void){
+
     uart_init();
     io_redirect();
+    sei(); // Enable global interrupts
 
-    DDRB |= (1 << DDB0);  //Set trigPin as output
-    DDRB &= ~(1 << DDB1);// Set echoPin as an input
-    PORTB |= (1 << PORTB1);// Enable internal pull-up resistor for echoPin
+    DDRB = 0b00100001;
+    PORTB = 0b00110010;
 
-	printf("%d", detect_ball());
-	printf("%d", detect_ball());
-	printf("%d", detect_ball());
-	printf("%d", detect_ball());
+    //Joystick configuration
+    ADMUX = (1<<REFS0); //Select vref = avcc
+    ADCSRA = (1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)|(1<<ADEN); //set prescaler to 128 and turn on adc module
 
-    return 0;
+    //motor configuration
+    DDRD |= (1 << DDD4) | (1 << DDD6); // Set dirPin1 and stepPin1 as output
+	DDRD |= (1 << DDD2) | (1 << DDD5); // Set dirPin2 and stepPin2 as output
+
+    joystick();
+
+    printf("done");
+    shotin = detect_ball();
+
+    if (shotin == 1){
+        printf("ball in");
+    }
+    if (shotin == 0){
+        printf("ball out");
+    }
+
+
+
+}
+
+void joystick (void){
+    joystickflag = 1;
+    while (joystickflag == 1){
+        adc_resultx = adc_read(JOYSTICK_X); //voltage depends on joystick stage so return voltage read (0-1024mV)
+        voltagex = (adc_resultx);
+        
+        adc_resulty = adc_read(JOYSTICK_Y); //voltage depends on joystick stage so return voltage read
+        voltagey = (adc_resulty);
+
+        if(voltagex >=1000){ //set threshold to start moving
+            move_right(600,200);
+            printf("moveright \n");
+        }
+        if(voltagex <=50){
+            move_left(600,200);
+            printf("moveleft \n");
+        }
+        if(voltagey >=1000){
+            move_up(600,200);
+            printf("moveup \n");
+        }
+        if(voltagey <=50){
+            move_down(600,200);
+            printf("movedown \n");
+        }
+
+        if (!(PINB & (1 << PINB4))) { // Button is active low, so it is pressed when the pin reads low
+            printf("Button pressed \n");
+            joystickflag = 0;
+        }
+    }
+    
+}
+
+void move_left(int steps, int delay){
+	PORTD |= (1 << PORTD4); // Set dirPin HIGH to move in a particular direction
+	PORTD |= (1 << PORTD2); // Set dirPin HIGH to move in a particular direction
+        for (int x = 0; x < steps; x++) {
+            PORTD |= (1 << PORTD6); // Set stepPin HIGH
+			PORTD |= (1 << PORTD5); // Set stepPin HIGH
+            _delay_us(delay);
+            PORTD &= ~(1 << PORTD6); // Set stepPin LOW
+			PORTD &= ~(1 << PORTD5); // Set stepPin LOW
+            _delay_us(delay);
+			}
+}
+void move_right(int steps, int delay){
+	PORTD &= ~(1 << PORTD4); // Set dirPin LOW to change direction of rotation
+	PORTD &= ~(1 << PORTD2);
+        for (int x = 0; x < steps; x++) {
+            PORTD |= (1 << PORTD6); // Set stepPin HIGH
+			PORTD |= (1 << PORTD5); // Set stepPin HIGH
+            _delay_us(delay);
+            PORTD &= ~(1 << PORTD6); // Set stepPin LOW
+			PORTD &= ~(1 << PORTD5); // Set stepPin LOW
+            _delay_us(delay);
+        }
+}
+
+void move_up(int steps, int delay){
+	PORTD |= (1 << PORTD4); // Set dirPin HIGH to move in a particular direction
+	PORTD &= ~(1 << PORTD2);
+        for (int x = 0; x < steps; x++) {
+            PORTD |= (1 << PORTD6); // Set stepPin HIGH
+			PORTD |= (1 << PORTD5); // Set stepPin HIGH
+            _delay_us(delay);
+            PORTD &= ~(1 << PORTD6); // Set stepPin LOW
+			PORTD &= ~(1 << PORTD5); // Set stepPin LOW
+            _delay_us(delay);
+        }
+}
+
+void move_down(int steps, int delay){
+	PORTD |= (1 << PORTD2); // Set dirPin HIGH to move in a particular direction
+	PORTD &= ~(1 << PORTD4);
+        for (int x = 0; x < steps; x++) {
+            PORTD |= (1 << PORTD6); // Set stepPin HIGH
+			PORTD |= (1 << PORTD5); // Set stepPin HIGH
+            _delay_us(delay);
+            PORTD &= ~(1 << PORTD6); // Set stepPin LOW
+			PORTD &= ~(1 << PORTD5); // Set stepPin LOW
+            _delay_us(delay);
+        }
+}
+
+uint16_t adc_read(uint8_t adc_channel){
+  ADMUX &= 0xf0; //clear any previously used channel keeping internal reference
+  ADMUX |= adc_channel; //set the desired channel 
+  ADCSRA |= (1<<ADSC); //start a conversion
+  while ((ADCSRA&(1<<ADSC))); //wait for conversion to complete
+  return ADC; //return result as a 16 bit unsigned int
 }
 
 void start_pulse() {
@@ -79,11 +221,11 @@ int detect_ball (void){
         distance = pulse_duration * 0.017 / 2;// Calculate the distance
         if (distance < 9.5) {
 			sensorflag = 1;
-			return (2); //Number of audio for "You scored a point"
+			return (1); //Number of audio for "You scored a point"
         }
 
         if (distance > 9.5 && counter>= 120) { //edit counter value for a longer time for shooting 
-            return (3); //Audio for "Try again to shoot" 
+            return (0); //Audio for "Try again to shoot" 
 			counter = 0;
         }
     }
